b8cdd91fc55c51140605209b53a30cd00be23bad
Tushar Verma <tuhsar2022@gmail.com>
2023-05-25 20:31:20 +0530

    submitting code-Thu May 25 2023 20:31:19 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -3,24 +3,17 @@ import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
-
-const [count,setCount]=useState(0)
-
-const handleCount=()=>{
-  setCount(count+1);
-
+const [input, setInput] = useState(0)
+const handleClick = ()=>{
+  setInput(input+1);
 }
-
-
 
   return (
     <div>
-        
-        <p>Button clicked {count} times</p>
-        <button onClick={handleCount}>Click Me</button>
+        <p>Button clicked {input} times</p>
+        <button onClick={handleClick}>Click me</button>
     </div>
   )
 }
-
 
 export default App
